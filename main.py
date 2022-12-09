2acfc18e932e5c1781cdd7f392d0835e46ee7759
Trentbyu <trentblack@byui.edu>
2022-12-08 22:19:11 -0700

    Cloud ability

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from source.game import Game
+from source.cloud.Stoarge import cloud
 
+
+CLOUD = cloud()
+
+CLOUD.download_file_from_bucket('DATA.csv', 'sample.csv')
 game = Game()
+CLOUD.upload_to_bucket('DATA.csv', "sample.csv")
